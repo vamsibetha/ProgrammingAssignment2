d2787e222eb0c15d37b76bb55d38a0472d367003
vamsibetha <vamsi.krishna.bethapudy@gmail.com>
2014-05-25 03:45:46 +0530

    Final Commit  clml

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,23 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is a lengthy and time consuming process. By caching 
+## the inverse matrix when it was first calculated, we can speed the 
+## computation of multiple inversion tasks. 
 
-## Write a short comment describing this function
+## The two functions given below are for the following purposes-
+
+## makeCacheMatrix - It defines the 4 functions for setting and getting 
+## the original matrix and its inverse. This function passes the handle
+## to any variable which can be easily used for these 4 purposes.
+## Here,
+## theMat == x (from the example)
+## passedMat == y
+## invMat == m
+## passedInvMat == mean
+## setinv() == setmean()
+## getinv() == getmean()
 
 makeCacheMatrix <- function(theMat = matrix()) {
   invMat <- NULL
+  
   set <- function(passedMat) {
     theMat <<- passedMat
     invMat <<- NULL
@@ -12,16 +25,24 @@ makeCacheMatrix <- function(theMat = matrix()) {
   get <- function() theMat
   setinv <- function(passedInvMat) invMat <<- passedInvMat
   getinv <- function() invMat
+  
   list(set = set, get = get,
        setinv = setinv,
        getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+## cacheSolve - This function simply takes the handle variable created
+## by the "makeCacheMatrix" function and then checks if the invMat is null.
+## If so, it calculates the invMat using the setinv() function. On 
+## subsequent calculations cacheSolve returns the already set value of 
+## invMat without actual calculations.
+## Here,
+## theMat == x (from the example)
+## invMat == m
+## setinv() == setmean()
+## getinv() == getmean()
 
 cacheSolve <- function(theMat, ...) {
-  ## Return a matrix that is the inverse of 'x'
   invMat <- theMat$getinv()
   if(!is.null(invMat)) {
     message("getting cached data")
